762af93cb521f00a827e603e29992232214ac337
AmirBazanov <flash5651311@gmail.com>
2024-05-23 09:00:08 +0300

    Add user in mobile footer

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -3,16 +3,24 @@
 import useConversation from "@/app/hooks/useConversation";
 import useRoutes from "@/app/hooks/useRoutes";
 import MobileItem from "./MobileItem";
-
-const MobileFooter = () => {
+import Avatar from "../Avatar";
+import { User } from "@prisma/client";
+import SettingsModal from "./SettingsModal";
+import { useState } from "react";
+interface DesktopSidebarProps {
+  currentUser: User
+}
+const MobileFooter: React.FC<DesktopSidebarProps>= ({currentUser}) => {
   const routes = useRoutes();
+  const [isOpenS,setIsOpen] = useState(false);
   const { isOpen } = useConversation();
-
+  console.log({ currentUser, }, 'TEST_MOBILE')
   if (isOpen) {
     return null;
   }
 
   return ( 
+    <> <SettingsModal currentUser={currentUser} isOpen={isOpenS} onClose={() => setIsOpen(false)} />
     <div 
       className="
         fixed 
@@ -36,7 +44,14 @@ const MobileFooter = () => {
           onClick={route.onClick}
         />
       ))}
+          <div 
+            onClick={() => setIsOpen(true)} 
+            className="cursor-pointer hover:opacity-75 transition pl-2 pr-2 pt-2"
+          >
+            <Avatar user={currentUser} />
+          </div>
     </div>
+    </>
    );
 }
  
